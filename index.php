01636f659489c8722bf98fa055324e78de55a180
atajan <atadzantajyrov020@gmail.com>
2020-08-20 07:29:06 -0700

    meta

--- index.php
+++ index.php
@@ -11,8 +11,7 @@
     <meta property="og:site_name" content="Atajan Website Developer"/>
     <meta property="og:description" content="Portfolio Website">
     <meta property="og:type" content="website">
-
-    <title>atajan.pro</title>
+    <title>Atajan - Website Developer</title>
     <link rel="stylesheet" href="css/style.css" />
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,669;0,700;0,800;0,900;1,400;1,500;1,600;1,669;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link
